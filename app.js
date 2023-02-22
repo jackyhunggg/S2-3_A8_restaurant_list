de3f911a3aa61a11271e35011338ad9fb336a2cd
Jacky Hung <jacky85725@gmail.com>
2023-02-22 19:05:21 +0800

    feat: add index

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const Restaurant = require('./models/restaurant')
 const app = express();
 const port = 3000;
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
 // 載入 mongoose
 const mongoose = require('mongoose')
-// 取得資料庫連線狀態
-const db = mongoose.connection
-
 // 設定連線到 mongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// 取得資料庫連線狀態
+const db = mongoose.connection
 // 連線異常
 db.on('error', () => {
     console.log('mongodb error!')
@@ -17,18 +21,17 @@ db.on('error', () => {
 db.once('open', () => {
     console.log('mongodb connected!')
 })
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
 
 app.use(express.static('public'));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars',exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // 使用者可以在首頁看到所有餐廳與它們的簡單資料
 app.get('/', (req, res) => {
-    res.render('index', {restaurant: list.results});
+    Restaurant.find()
+        .lean()
+        .then(restaurantData => res.render('index', { restaurantData }))
+        .catch(err => console.log(err))
 })
 
 // 使用者可以透過搜尋餐廳名稱and類別來找到特定的餐廳
